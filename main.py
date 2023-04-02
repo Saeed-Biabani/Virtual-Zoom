a8a0354ad3b3b5040158140124f406f4f02ae77e
Saeed Biabani <saeedbiabanii@gmail.com>
2023-04-02 19:04:42 +0330

    detect hands, get Hosts read image

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import cv2
 from cvzone.HandTrackingModule import HandDetector
 from sys import argv
 
+
+MINDIST = 35
+SPECIAL_POINTS = {}
+
+
+fname = argv[1]
+OriginalIMG = cv2.imread(fname)
+ROI = OriginalIMG.copy()
+
 cap = cv2.VideoCapture(1)
 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
@@ -15,6 +24,42 @@ while True:
     FlipedFrame = cv2.flip(frame, 1)
 
     Hands, DrawedFrame = Detector.findHands(FlipedFrame, draw = True, flipType = False)
+
+    if Hands:
+        Left = Hands[0]
+        Left_LmList = Left['lmList']
+        
+        True_Left_Distance = int(Detector.findDistance(Left_LmList[4][:2], Left_LmList[8][:2])[0])
+
+        try:
+            Right = Hands[1]
+            Right_LmList = Right['lmList']
+            
+            True_Right_Distance = int(Detector.findDistance(Right_LmList[4][:2], Right_LmList[8][:2])[0])
+
+            if True_Left_Distance <= MINDIST and True_Right_Distance <= MINDIST:
+                Left_Host = Left['center']
+                Right_Host = Right['center']
+   
+
+                SPECIAL_POINTS.update({"START": Left_Host})
+                SPECIAL_POINTS.update({"END": Right_Host})
+                
+                cv2.rectangle(FlipedFrame, Left_Host, Right_Host, (0, 255, 255), 5)
+                
+                x1, y1 = Left_Host
+                x2, y2 = Right_Host
+                
+                x_min = min(x1, x2)
+                x_max = max(x1, x2)
+
+                y_min = min(y1, y2)
+                y_max = max(y1, y2)
+                
+                FlipedFrame[y_min:y_max, x_min:x_max] = cv2.resize(OriginalIMG, (abs(x2 - x1), abs(y2 - y1)))
+        
+        except:
+            pass
 
     cv2.imshow("Virtual Zoom", DrawedFrame)
 
