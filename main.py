456edff0c3b419562e99f65409b18f541b174321
Saeed Biabani <saeedbiabanii@gmail.com>
2023-04-02 18:59:21 +0330

    add Hand Detector

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ cap = cv2.VideoCapture(1)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
+Detector = HandDetector(maxHands = 2, detectionCon = 0.8)
+
 while True:
     re, frame = cap.read()
 
-    cv2.imshow("Virtual Zoom", frame)
+    FlipedFrame = cv2.flip(frame, 1)
+
+    Hands, DrawedFrame = Detector.findHands(FlipedFrame, draw = True, flipType = False)
+
+    cv2.imshow("Virtual Zoom", DrawedFrame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
